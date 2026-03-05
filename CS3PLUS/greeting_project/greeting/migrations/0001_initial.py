d45cbe76410a7a74ea8cee2760f9e58eefcbdb4b
SkiF4er <cajeerskif4er@gmail.com>
2026-03-05 20:42:27 +0300

    Add files via upload

--- CS3PLUS/greeting_project/greeting/migrations/0001_initial.py
+++ CS3PLUS/greeting_project/greeting/migrations/0001_initial.py
@@ -1,1 +1,24 @@
+# Generated manually for the educational project (equivalent to makemigrations output).
 
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = []
+
+    operations = [
+        migrations.CreateModel(
+            name="Greeting",
+            fields=[
+                ("id", models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name="ID")),
+                ("name", models.CharField(max_length=100, verbose_name="Name")),
+                ("created_at", models.DateTimeField(auto_now_add=True, verbose_name="Created at")),
+            ],
+            options={
+                "ordering": ["-created_at"],
+            },
+        ),
+    ]
